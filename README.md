a6db4f61b3a8f238b61d80e69a709cd2a7f79031
Tobias Meyer <52013820+Hope-IT-Works@users.noreply.github.com>
2024-03-08 17:12:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# playwright-template
+
+Template repository for playwright projects
+
+## Usage
+
+1. Create a new repository from this template → [**Use template**](https://github.com/new?template_name=playwright-template&template_owner=Hope-IT-Works)
+2. Clone your new repository
+3. Navigate to the cloned repository
+4. Run `npm install`
---
+++ src/playwright.config.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'list',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure project for chrome */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ]
+});
+
